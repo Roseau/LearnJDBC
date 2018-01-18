d43ada632e34df1fd75b95705ff2e14f5f6e05d6
Roseau <asboyo@gmail.com>
2018-01-18 10:26:28 +0700

    latihan JDBC

---
+++ src/main/java/com/gmail/asboyo/learn/jdbc/AplikasiUtama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.asboyo.learn.jdbc;
+
+/**
+ *
+ * @author St0rm
+ */
+public class AplikasiUtama {
+    public static void main(String[] args) {
+        System.out.println("unga bunga. ini aplikasi maven pertama!");
+        System.out.println("unga bunga. its another line!");
+        for(int i=0;i<11;i++){
+            System.out.print("(*-*)/");
+        }
+    }
+}
