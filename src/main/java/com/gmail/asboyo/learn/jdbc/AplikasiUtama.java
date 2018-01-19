f5e285fdd02cd169f9d0b340eaa24e93e0be7ba3
Roseau <asboyo@gmail.com>
2018-01-19 07:51:23 +0700

    test input dan delete ke dan dari database. multiple commit found

--- src/main/java/com/gmail/asboyo/learn/jdbc/AplikasiUtama.java
+++ src/main/java/com/gmail/asboyo/learn/jdbc/AplikasiUtama.java
@@ -10,28 +10,73 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.dbcp2.BasicDataSource;
+import entity.Department;
+import dao.DepartmentDAO;
+import java.util.List;
 
 /**
  *
  * @author St0rm
  */
 public class AplikasiUtama {
-    private final String url = "jdbc:postgresql://localhost:5432/hr";
-    private final String user = "hr";
-    private final String password = "hr";
-    
-    public Connection connect(){
-        Connection conn = null;
-            try {
-                conn = DriverManager.getConnection(url,user,password);
-                System.out.println("Berhasil Connect");
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());;
+//    private final String url = "jdbc:postgresql://localhost:5432/hr";
+//    private final String user = "hr";
+//    private final String password = "hr";
+//    
+//    public Connection connect(){
+//        Connection conn = null;
+//            try {
+//                conn = DriverManager.getConnection(url,user,password);
+//                System.out.println("Berhasil Connect");
+//            } catch (SQLException ex) {
+//                System.out.println(ex.getMessage());;
+//            }
+//        return conn;
+//    }
+    public static void koneksi(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUsername("hr");
+        ds.setPassword("hr");
+        ds.setUrl("jdbc:postgresql://localhost:5432/hr");
+        ds.setDriverClassName("org.postgresql.Driver");
+
+        Connection connection = null;
+        try{
+            // membuka koneksi ke database
+            connection = ds.getConnection();
+            connection.setAutoCommit(false);
+            System.out.println("berhasil koneksi ke database");
+            DepartmentDAO dao = new DepartmentDAO(connection);
+
+            //save nilai department
+            Department departmentBaru = dao.Save(new Department(null, "Sistem Analis", 1000, null));
+            System.out.println(departmentBaru.toString());
+      
+            dao.Save(new Department(null, "Sistem Analis", 1000, null));
+            
+            dao.delete(3003);
+            //untuk ambil nilainya
+            List<Department> daftarDepartment = dao.findAll();
+            for (Department d : daftarDepartment) {
+                System.out.println(d.toString());
             }
-        return conn;
+            connection.commit();
+            connection.close();
+        }catch(SQLException sqle){
+            System.err.printf("tidak dapat koneksi ke databas!");
+            sqle.printStackTrace();
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+               ex.printStackTrace();
+            }
+        }
     }
+    
     public static void main(String[] args) {
-        AplikasiUtama ap = new AplikasiUtama();
-        ap.connect();
+        koneksi();
     }
 }
